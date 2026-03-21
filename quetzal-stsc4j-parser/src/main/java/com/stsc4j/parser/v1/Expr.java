3a8ac9225e10a0b19da0b36e11f61cd8bf01c460
Alexander Machic <alexandevcwa@gmail.com>
2026-03-20 19:30:19 -0600

    Delete unnecessary classes that v1 project package can cover functionalities

--- quetzal-stsc4j-parser/src/main/java/com/stsc4j/parser/v1/Expr.java
+++
@@ -1,5 +0,0 @@
-package com.stsc4j.parser.v1;
-
-public abstract class Expr {
-    abstract <R> R accept(Visitor<R> visitor);
-}
--- quetzal-stsc4j-parser/src/main/java/com/stsc4j/parser/v1/Visitor.java
+++
@@ -1,6 +0,0 @@
-package com.stsc4j.parser.v1;
-
-public interface Visitor<R> {
-    R visitLiteralExpr(Literal expr);
-    R visitBinaryExpr(Binary expr);
-}
