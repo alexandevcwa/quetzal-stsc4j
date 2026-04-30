9a021cb398515770bb1cf4874c39044750f9284b
Alexander Machic <alexandevcwa@gmail.com>
2026-04-30 00:49:03 -0600

    Añade soporte para acceso a índices múltiples en matrices: actualiza `ParserExpressions` y `ASTPrinter`, introduce constructor para múltiples índices en `ExpressionIndexAccess`, y añade nuevos tests unitarios para matrices unidimensionales y bidimensionales. FIXED #16

--- quetzal-stsc4j-parser/src/main/java/com/stsc4j/parser/v1/ast/ExpressionIndexAccess.java
+++ quetzal-stsc4j-parser/src/main/java/com/stsc4j/parser/v1/ast/ExpressionIndexAccess.java
@@ -1,13 +1,22 @@
 package com.stsc4j.parser.v1.ast;
 
+import java.util.List;
+
 public class ExpressionIndexAccess extends Expression{
 
     public Expression objectList;
+    public List<Expression> indexList;
     public Expression index;
 
+    @Deprecated
     public ExpressionIndexAccess(Expression objectList, Expression index) {
         this.objectList = objectList;
         this.index = index;
+    }
+
+    public ExpressionIndexAccess(Expression objectList, List<Expression> indexList) {
+        this.objectList = objectList;
+        this.indexList = indexList;
     }
 
     @Override
